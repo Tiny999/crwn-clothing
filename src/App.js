0f296c58a4d5c573cbcae2d32ad5feada46bf6ce
Abbas Salami <olawalesalami3@gmail.com>
2020-11-13 22:33:36 +0100

    sign-in and button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import Homepage from './pages/Homepage/homepage';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/header/header';
+import SignInSignUp from './pages/signInSignUp/signInSignUp';
 
 function App(){
   return (
@@ -13,6 +14,7 @@ function App(){
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp}/>
       </Switch>
     </div>
   );
